60f3a945f4a9fd5060624006dc0b451385eca5bd
buj <buj351@outlook.com>
2025-05-01 14:19:11 +0700

    added buncha shit

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
-sudo pacman -Syy kitty zsh starship hyprland rustup git curl wget \
-                 cmake nerd-fonts hyprpaper eza waybar \
-                 hyprpolkitagent lua-language-server \
+sudo pacman -Syy ghostty zsh starship hyprland rustup git curl wget \
+                 cmake nerd-fonts hyprpaper eza waybar cliphist \
+                 hyprpolkitagent lua-language-server iwd \
                  typescript-language-server jdk{,21,8}-openjdk -y
 chsh -s /usr/bin/zsh
 git clone https://github.com/ohmyzsh/ohmyzsh/ ~/.oh-my-zsh
@@ -10,5 +10,7 @@ useradd -mUr pkgbuild
 echo 'pkgbuild ALL=(ALL:ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 sudo -iupkgbuild sh -c 'git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si'
 sudo -iupkgbuild paru -S opentabletdriver jdtls sonarlint-ls-bin -y
+
+systemctl enable iwd
 
 exec ./update.sh
