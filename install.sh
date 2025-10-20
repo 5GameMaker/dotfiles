35cf1e704a425b40ef43f9725915b06be36ffd18
buj <buj351@outlook.com>
2025-10-21 01:52:55 +0700

    fixing install script + hyprland broke my config files again

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/sh
 
-sudo pacman -Syy ghostty zsh starship hyprland rustup git curl wget \
+sudo pacman -Syy kitty zsh starship hyprland rustup git curl wget \
                  cmake nerd-fonts hyprpaper eza waybar cliphist \
-                 hyprpolkitagent lua-language-server iwd \
-                 typescript-language-server jdk{,21,8}-openjdk marksman -y
+                 hyprpolkitagent lua-language-server \
+                 typescript-language-server jdk{,21,8}-openjdk marksman \
+                 pipewire pipewire-pulse pipewire-jack wireplumber \
+                 xdg-desktop-portal-{hyprland,kde} xorg-xwayland
+
 chsh -s /usr/bin/zsh
 git clone https://github.com/ohmyzsh/ohmyzsh/ ~/.oh-my-zsh
 useradd -mUr pkgbuild
 echo 'pkgbuild ALL=(ALL:ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+sudo -iupkgbuild rustup toolchain stable
 sudo -iupkgbuild sh -c 'git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si'
 sudo -iupkgbuild paru -S opentabletdriver jdtls sonarlint-ls-bin kotlin-language-server -y
 
-systemctl enable iwd
+systemctl enable --user pipewire
+systemctl enable --user pipewire-pulse
+systemctl enable --user wireplumber
+systemctl enable --user opentabletdriver
 
 exec ./update.sh
